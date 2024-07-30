cea6c7416f5285643c7837bea150b14c3404bc00
Taylor Albright <jtayloralbright@gmail.com>
2024-07-30 16:36:40 -0500

    changed mapping to appendChild

--- scripts/fishList.mjs
+++ scripts/fishList.mjs
@@ -4,8 +4,13 @@ export const fishList = () => {
     // Generate an HTML representation of each fish
     const fishListElement = document.querySelector('#fishList')
     database.fish.map(fish => {
-        fishListElement.innerHTML += `<h2>${fish.species}</h2>`
+        const h2 = document.createElement('h2')
+        h2.textContent = `${fish.species}`
+        fishListElement.appendChild(h2)
     })
+    // database.fish.map(fish => {
+    //     fishListElement.innerHTML += `<h2>${fish.species}</h2>`
+    // })
 
 
 };
